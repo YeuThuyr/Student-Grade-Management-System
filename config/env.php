92689c8975521025451d7952fd7251291975746f
YeuThuyr <truongxuan220204@gmail.com>
2026-05-30 13:24:30 +0700

    site error-reporting

--- config/env.php
+++ config/env.php
@@ -1,4 +1,9 @@
 <?php
+// Temporarily enable error reporting to diagnose the 500 error on the live server
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if (!defined('BASE_PATH')) {
     $scriptDir = dirname($_SERVER['SCRIPT_NAME']);
     // Replace backslashes with forward slashes for Windows compatibility
